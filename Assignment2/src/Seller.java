15a0d0698083a762a85136fe300626c58293a513
Derek <derekt.ly@gmail.com>
2013-10-17 23:30:31 -0500

    Adding an incomplete "addItem()" method in the Seller class. Testing how git works on eclipse

--- Assignment2/src/Seller.java
+++ Assignment2/src/Seller.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
+
 
 public class Seller {
-
+	private Brokers parentBroker;
+	private String sellerID;
+	
+	public void addItem(){
+		String item;
+		
+		System.out.println("What is the name of your item");
+		Scanner input = new Scanner(System.in); 
+		item = input.next();
+		while (parentBroker.isItemInList(item)){
+			System.out.println("There already exists an item with that name, please enter another name");
+			item = input.next();
+		}
+		
+	
+	}
+		
 }
